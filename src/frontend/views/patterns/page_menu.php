c74495ad9d683476837f97622abeaacf3e028aef
Alex Flitcher <76988822+alexflitcher1@users.noreply.github.com>
2022-02-28 17:03:56 +0300

    Update page_menu.php

--- src/frontend/views/patterns/page_menu.php
+++ src/frontend/views/patterns/page_menu.php
@@ -5,69 +5,69 @@ use yii\widgets\ActiveForm;
 <div class="page_menu_sticky">
 				<div class="profile_names">
 					<div class="profile_names_left">
-						<img src="/<?=Html::encode($user->img)?>" class="profile_names_ava">
+						<img src="/<?=Html::encode($thisuser->img)?>" class="profile_names_ava">
 					</div>
 					<div class="profile_names_right">
-						<div class="profile_names_username"><?=Html::encode($user->username)?></div>
-						<div class="profile_names_name"><?=Html::encode($user->name)?></div>
+						<div class="profile_names_username"><?=Html::encode($thisuser->username)?></div>
+						<div class="profile_names_name"><?=Html::encode($thisuser->name)?></div>
 					</div>
 				</div>
 				<div class="page_menu_bio">
 					<div class="page_menu_bio_str">
-						<?php if (strlen($user->about)): ?>
+						<?php if (strlen($thisuser->about)): ?>
 							<span class="page_menu_bio_str_name">О себе</span>
-							<?=Html::encode($user->about)?>
+							<?=Html::encode($thisuser->about)?>
 						<?php endif; ?>
 					</div>
 					<div class="page_menu_bio_str">
 						<span class="page_menu_bio_str_name">Пол</span>
-						<?php if (Html::encode($user->gender) == 'woman'): ?>
+						<?php if (Html::encode($thisuser->gender) == 'woman'): ?>
 							<?=Html::encode('Женский')?>
-						<?php elseif (Html::encode($user->gender) == 'man'): ?>
+						<?php elseif (Html::encode($thisuser->gender) == 'man'): ?>
 							<?=Html::encode('Мужской')?>
 						<?php else: ?>
 							<?=Html::encode('Другой')?>
 						<?php endif; ?>
 					</div>
 					<div class="page_menu_bio_str">
-						<?php if (strlen($user->city)): ?>
+						<?php if (strlen($thisuser->city)): ?>
 							<span class="page_menu_bio_str_name">Город</span>
-							<?=Html::encode($user->city)?>
+							<?=Html::encode($thisuser->city)?>
 						<?php endif; ?>
 					</div>
 					<div class="page_menu_bio_str">
-						<?php if (strlen($user->site)): ?>
+						<?php if (strlen($thisuser->site)): ?>
 							<span class="page_menu_bio_str_name">Сайт</span>
-							<a href="https://<?=Html::encode($user->site)?>">
-								<?=Html::encode($user->site)?>
+							<a href="https://<?=Html::encode($thisuser->site)?>">
+								<?=Html::encode($thisuser->site)?>
 							</a>
 						<?php endif; ?>
 					</div>
 					<div class="page_menu_bio_str">
-						<?php if (strlen($user->telegram)): ?>
+						<?php if (strlen($thisuser->telegram)): ?>
 							<span class="page_menu_bio_str_name">Telegram</span>
-							<a href="https://t.me/<?=Html::encode($user->telegram)?>">
-								<?=Html::encode($user->telegram)?>
+							<a href="https://t.me/<?=Html::encode($thisuser->telegram)?>">
+								<?=Html::encode($thisuser->telegram)?>
 							</a>
 						<?php endif; ?>
 					</div>
 					<div class="page_menu_bio_str">
 						<span class="page_menu_bio_str_name">Дата Регистрации</span>
-						<?=Html::encode(date("d.m.Y", strtotime($user->regdate)))?>
+						<?=Html::encode(date("d.m.Y", strtotime($thisuser->regdate)))?>
 					</div>
 				</div>
 				<div class="fl_menu">
 					<div class="fl_menu_block">
 						<div class="fl_menu_n"><?=Html::encode($subs)?></div>
-						<div class="fl_menu_name"><a href="/subscribers?id=<?=$user->id?>">Подписчики</a></div>
+						<div class="fl_menu_name"><a href="/subscribers?id=<?=$thisuser->id?>">Подписчики</a></div>
 					</div>
 					<div class="fl_menu_block">
 						<div class="fl_menu_n"><?=Html::encode($suber)?></div>
-						<div class="fl_menu_name"><a href="/suber?id=<?=$user->id?>&mode=1">Подписки</a></div>
+						<div class="fl_menu_name"><a href="/suber?id=<?=$thisuser->id?>&mode=1">Подписки</a></div>
 					</div>
 					<div class="fl_menu_block">
 						<div class="fl_menu_n"><?=Html::encode(count($posts))?></div>
-						<div class="fl_menu_name"><a href="/<?=$user->username?>">Твиты</a></div>
+						<div class="fl_menu_name"><a href="/<?=$thisuser->username?>">Твиты</a></div>
 					</div>
 				</div>
 				<div class="page_menu_nav">
